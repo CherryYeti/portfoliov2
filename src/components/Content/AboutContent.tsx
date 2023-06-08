b0050a9608ee10407c87be355832d17ad4cb40c8
Jason Javandel <jasonjavandel@protonmail.com>
2023-06-08 00:24:22 -0700

    Change some wording

--- src/components/Content/AboutContent.tsx
+++ src/components/Content/AboutContent.tsx
@@ -2,7 +2,7 @@ import "./AboutContent.css"
 function AboutContent(){
     return(
         <div className="aboutContent">
-            <h2>Hello! I am an 18 year old freshman at the University of California, Merced who believes in open source projects. I am majoring in computer science, and I love creating!</h2>
+            <h2>Hello! I am a second year college student who believes in open source projects. I am majoring in computer science, and I love creating!</h2>
         </div>
     )
 }
